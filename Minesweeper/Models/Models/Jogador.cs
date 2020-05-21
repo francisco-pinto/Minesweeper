854d1a97dc0501296fcc19ba210532a41e6150e3
Gonçalo Penelas <al68536@utad.eu>
2020-05-21 15:15:58 +0100

    Serialização

--- Minesweeper/Models/Models/Jogador.cs
+++ Minesweeper/Models/Models/Jogador.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Minesweeper
 {
@@ -17,15 +19,42 @@ namespace Minesweeper
 
         Jogador jogador = new Jogador();
 
-        //var jogadorQuery =
-        //    from jogador in Jogador
-        //    select new { Jogador.nome, jogador.pontuacao };
+        ////Serializaçao
+
+        ////escrever
+        //FileStream fs = new FileStream("jogador.xml", FileMode.Create);
+
+        //XmlSerializer xs = new XmlSerializer(typeof(Jogador));
+        //xs.Serialize(FileStream, jogador);
+        
+        //fs.Close();
+
+        ////ler
+        //FileStream fs = new FileStream("jogador.xml", FileMode.Open);
+
+        //XmlSerializer xs = new XmlSerializer(typeof(Jogador));
+        //Jogador jogador = (Jogador)xs.Deserialize(fs);
+
+        //fs.close();
+
+
+        ////Linq
+
+        //var jogadores = new Jogador[]
+        //{
+        //    new Jogador { Nome = "aaaaaa", Pontuacao = 999}
+        //};
 
         //var jogadorQuery =
-        //{
-        //    new Jogador { nome="aaaaa", pontuacao=999}
-        //};
-        
-        
+        //    from jogador in jogadores
+        //    select new { jogador.Nome, jogador.Pontuacao };
+
+        //foreach (var jogador in jogadorQuery)
+        //    {
+        //        Console.WriteLine("{0}", jogador.Nome);
+        //        Console.WriteLine("{0}", jogador.Pontuacao);
+        //    }
+
+
     }
 }
