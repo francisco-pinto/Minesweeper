fd6cced1636c88f36d9bd2514191afc8e7b8998c
Samuel Félix <23samuel12felix2000@gmail.com>
2020-05-28 23:01:29 +0100

    Foto Menu

--- Minesweeper/Minesweeper/View_Controller/FormMenu.cs
+++ Minesweeper/Minesweeper/View_Controller/FormMenu.cs
@@ -9,7 +9,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Menu = Minesweeper.Models.Menu; //diferenciar o menu classe do form.menu
-
+using System.Net;
+using System.IO;
+using System.Xml.Linq;
 
 namespace Minesweeper.View_Controller
 {
@@ -179,7 +181,39 @@ namespace Minesweeper.View_Controller
         }
         public void ShowFoto()
         {
-            
+            //Prepara o pedido ao servidor com o URL adequado
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://prateleira.utad.priv:1234/LPDSW/2019-2020/perfil/" + "xcoelho");
+
+            // Com o acesso usa HTTPS e o servidor usar cerificados autoassinados, tempos de configurar o cliente para aceitar sempre o certificado.
+            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
+
+            request.Method = "GET"; // método usado para enviar o pedido
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); // faz o envio do pedido
+
+            Stream receiveStream = response.GetResponseStream(); // obtem o stream associado à resposta.
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8); // Canaliza o stream para um leitor de stream de nível superior com o
+                                                                                      //formato de codificação necessário.
+            string resultado = readStream.ReadToEnd();
+
+            response.Close();
+            readStream.Close();
+
+            // converte para objeto XML para facilitar a extração da informação e ...
+            XDocument xmlResposta = XDocument.Parse(resultado);
+
+            string base64Imagem = xmlResposta.Element("resultado").Element("objeto").Element("perfil").Element("fotografia").Value;
+            string base64 = base64Imagem.Split(',')[1]; // retira a parte da string correspondente aos bytes da imagem..
+            byte[] bytes = Convert.FromBase64String(base64); //...converte para array de bytes...
+            Image image = Image.FromStream(new MemoryStream(bytes));//... e, por fim, para Image
+
+            // pode mostrar a imagem num qualquer componente...como por exemplo:
+            pictureBoxFotoMenu.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBoxFotoMenu.BackgroundImage = image;
+        }
+        public bool AcceptAllCertifications(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certification, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
         }
     }
 }
